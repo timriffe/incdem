e5fbeba94d690f10130b685216a21d079f0053ad
timriffe <timriffe@gmail.com>
2025-05-28 15:55:41 +0200

    adds source of renv/activate.R to profile in project, meaning it actives installation as soon as a project is opened.

--- R/0_packages.R
+++ R/0_packages.R
@@ -1,6 +1,10 @@
+
+cran_packages <- c("tidyverse","splines","msm")
 # ------------------------------------------------------------------- #
 # install.packages("msm")
 library(tidyverse)
 library(splines)
 library(msm)
 # ------------------------------------------------------------------- #
+
+# renv::snapshot()
