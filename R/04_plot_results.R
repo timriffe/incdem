09942c5f2ad963a655f6020c3ff7f84c89ff29e1
timriffe <timriffe@gmail.com>
2025-06-04 15:46:58 +0200

    modifies first figure

--- R/04_plot_results.R
+++ R/04_plot_results.R
@@ -7,10 +7,12 @@ result_df <- read_csv("Data/results.csv.gz")
 # ------------------------------------------------------------------- #
 # plot rates
 result_df |>
-  mutate(Period = as.factor(int_date_decimal)) |>
+  filter(parse_number(to) > parse_number(from)) |> 
+  mutate(Period = as.factor(int_date_decimal),
+         transition = paste0("haz",parse_number(from),parse_number(to))) |>
   ggplot(aes(x        = age, 
              y        = rate, 
-             color    = to, 
+             color    = transition, 
              linetype = Period)) +
   geom_line(linewidth = 1) +
   labs(
@@ -22,7 +24,7 @@ result_df |>
   ) +
   theme_minimal() +
   scale_y_log10() +
-  facet_wrap(sex ~ from) + 
+  facet_wrap(~sex) + 
   theme(legend.position = "bottom")
 # ------------------------------------------------------------------- #
 # plot probability
