ec0235ba958997b0d9b39bf8f76fe19efd5fef18
timriffe <tim.riffe@gmail.com>
2025-06-10 16:49:16 +0200

    .

---
+++ R/05_empirical_prevalence.R
@@ -0,0 +1,17 @@
+
+# placeholder script.
+# Here we should estimate the empirical prevalence,
+# and if this isn't too space-consuming also calculate
+# the stationary prevalence. Finally, also each of these
+# with univariate covariates.
+
+
+
+
+
+
+
+
+
+
+
