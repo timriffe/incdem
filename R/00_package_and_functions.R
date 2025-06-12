068ee3722989ea70f6d783be80d3c19d4469fc13
timriffe <tim.riffe@gmail.com>
2025-06-12 10:03:18 +0200

    second Q fix

--- R/00_package_and_functions.R
+++ R/00_package_and_functions.R
@@ -103,9 +103,7 @@ fit_msm_models <- function(.data,
                                   c(0, -.01,   0.1),  # dementia can go to death
                                   c(0, 0,   0)        # death is absorbing
                                 )) {
-  
-  # finilize the Q matrix
-  diag(Q) <- -rowSums(Q)
+
   
   # --------------------------------------------------------------#
   # PT1
