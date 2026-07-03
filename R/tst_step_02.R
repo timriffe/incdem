8a26a47329d4639f70533d3a04e030cd93eaf211
Rustam <rustam.tursunzade@gmail.com>
2026-07-03 15:35:41 +0200

    added preparatory step test

---
+++ R/tst_step_02.R
@@ -0,0 +1,400 @@
+# Load required packages
+source("R/00_dependencies.R")
+source("R/01_functions.R")
+# Select necessary variables from main dataset
+max_wave <- 16
+hrs_file <- if_else(max_wave == 16, 
+                    "randhrs1992_2022v1.dta",
+                    "randhrs1992_2020v2.dta")
+hrs_vars <- c("hhidpn", "hhid", "pn", 
+              "hacohort", "rabdate", "raddate",
+              paste0("r", 1:max_wave, "iwstat"),
+              paste0("r", 1:max_wave, "iwmid"),
+              paste0("r", 1:max_wave, "hibpe"),
+              paste0("r", 1:max_wave, "diabe"),
+              paste0("r", 1:max_wave, "hearte"),
+              paste0("r", 1:max_wave, "stroke"),
+              paste0("r", 1:max_wave, "wtcrnh"),
+              "ragender", "raracem", "rahispan", "rabyear", "raeduc")
+
+# Load main RAND HRS dataset
+hrs_in <- read_dta(file.path("Data",hrs_file), 
+                   encoding   = "UTF-8", 
+                   col_select = all_of(hrs_vars)) |> 
+  zap_labels() |> 
+  mutate(
+    hhid   = as.character(hhid),
+    pn     = as.character(pn),
+    hhidpn = paste0(hhid, pn)
+  )
+
+# Load cognition dataset
+cog_colnames <- names(read_dta("Data/cogfinalimp_9520wide.dta", n_max = 0))
+cog_cols_needed <- c("hhid", "pn", grep("^cogfunction\\d{4}$", 
+                                        cog_colnames, 
+                                        value = TRUE))
+
+cog <- read_dta("./Data/cogfinalimp_9520wide.dta", 
+                encoding   = "UTF-8", 
+                col_select = all_of(cog_cols_needed)) |>
+  zap_labels() |>
+  mutate(
+    hhid   = as.character(hhid),
+    pn     = as.character(pn),
+    hhidpn = paste0(hhid, pn)
+  ) |>
+  select(hhidpn, starts_with("cogfunction"))
+
+# HERE WE GO AGAIN. more missing in latest waves
+hrs_long %>% 
+  filter(is.na(wtcrnh)) %>% 
+  count(wave)
+
+# but for dead people relatively stable. hmmm
+hrs_long %>% 
+  filter(is.na(wtcrnh), iwstat == 5) %>% 
+  count(wave)
+
+# UP UNTIL HERE NOTHING HAPPENS
+# Pre-process HRS dataset
+hrs_long <- hrs_in |>
+  mutate(
+    female = as.integer(ragender == 2),
+    race   = case_when(
+      raracem == 1 ~ "white",
+      raracem == 2 ~ "black",
+      raracem == 3 ~ "other"
+    ),
+    hispanic = if_else(rahispan == 1, "hispanic", "non-hispanic"),
+    education = case_when(
+      raeduc %in% c(1, 2) ~ "ls hs/ged",
+      raeduc == 3         ~ "hs",
+      raeduc %in% c(4, 5) ~ ">hs"
+    ),
+    birth_year = rabyear,
+    cohort     = hacohort
+  ) |>
+  pivot_longer(
+    cols = matches("^r\\d{1,2}(iwstat|iwmid|hibpe|diabe|hearte|stroke|wtcrnh)$"),
+    names_to      = c("wave", ".value"),
+    names_pattern = "r(\\d{1,2})([a-z]+)"
+  ) |>
+  mutate(wave = as.integer(wave)) |>
+  filter(iwstat %in% c(1, 4, 5))
+
+names(hrs_long) %>% sort()
+# Now we check
+# how many na in raddate WHEN iwstat == 5. ANSWER 191
+sum(is.na(hrs_long$raddate[hrs_long$iwstat == 5]))
+# so it is a very very low number and should not be significant
+sum(is.na(hrs_long$raddate[hrs_long$iwstat == 5])) / nrow(hrs_long)
+
+# maybe they all are in 1 wave?
+# doesnt look like it. yes, in wave 14 we have 42 missing,
+# but it is still low
+hrs_long %>% 
+  filter(iwstat == 5, is.na(raddate)) %>%
+  count(wave)
+
+# again nothing interesting
+hrs_long1 <- hrs_long |>
+  mutate(
+    age_interview = (iwmid - rabdate) / 365.25,
+    age_death     = (raddate - rabdate) / 365.25,
+    age           = if_else(iwstat == 5, age_death, age_interview),
+    int_date      = if_else(iwstat == 5, raddate, iwmid)
+  ) |>
+  group_by(hhidpn) |> 
+  mutate(n = n()) |> 
+  ungroup() |> 
+  filter(n > 1) |> 
+  arrange(hhidpn, wave)
+
+# age imputation 43903 missing or
+is.na(hrs_long1$age) %>% sum()
+
+# HERE WE GO. More missing info on age in latest waves
+hrs_long1 %>% 
+  filter(is.na(age)) %>% 
+  count(wave)
+
+hrs_long1 %>% 
+  group_by(wave) %>% 
+  summarise(n = n())
+
+# how many of these are deaths? not much
+hrs_long1 %>% 
+  filter(iwstat == 5, is.na(age)) %>% 
+  count(wave)
+
+# distribution of imputed ages. Most are 50-60-80. age_imputed = 3 missing
+hrs_long1 |>
+  group_by(hhidpn) |>
+  mutate(age_imputed = impute_age(age, wave)) |>
+  filter(is.na(age)) |>
+  ggplot(aes(x = age_imputed)) +
+  geom_density(fill = "grey70", alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(
+    x = "Imputed age",
+    y = "Density"
+  )
+
+# imputed deaths shift to 70-90
+hrs_long1 |>
+  group_by(hhidpn) |>
+  mutate(age_imputed = impute_age(age, wave)) |>
+  filter(is.na(age), iwstat == 5) |>
+  ggplot(aes(x = age_imputed)) +
+  geom_density(fill = "grey70", alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(
+    x = "Imputed age",
+    y = "Density"
+  )
+
+# by wave. except wave 13 look fine. only 3 imputations in wave 13
+hrs_long1 |>
+  group_by(hhidpn) |>
+  mutate(age_imputed = impute_age(age, wave)) |>
+  filter(is.na(age), iwstat == 5) |>
+  ggplot(aes(x = age_imputed)) +
+  geom_density(fill = "grey70", alpha = 0.5) +
+  theme_bw(base_size = 12) +
+  labs(
+    x = "Imputed age",
+    y = "Density"
+  )+
+  facet_wrap(~ wave)
+
+
+hrs_long2 <- hrs_long1 %>% 
+  group_by(hhidpn) |>
+  mutate(age_imputed = impute_age(age, wave)) |> 
+  ungroup() |>
+  group_by(hhidpn) |>
+  mutate(across(c(hibpe, diabe, hearte, stroke),
+                ~ cummax(replace_na(., 0)),
+                .names = "{.col}_status")) |>
+  ungroup()
+
+# NOTHING INTERESTING
+# Cognition: reshape and process
+cog_long <- cog |>
+  pivot_longer(
+    cols          = starts_with("cogfunction"),
+    names_to      = "year",
+    names_pattern = "cogfunction(\\d{4})",
+    values_to     = "cogfunction"
+  ) |>
+  mutate(year = as.integer(year)) |>
+  # ignore cog status before 2000; inconsistent measurement
+  # until 2000+
+  # but note, there are 2335 cases of a 'first' dementia observed
+  # before 2000. a possible sensitivity analyses
+  filter(year >= 2000) |> 
+  arrange(hhidpn,year) |> 
+  # Recode: 1/2 = 0, 3 = 1
+  mutate(cog_dementia = case_when(
+    cogfunction %in% 1:2 ~ 0L,
+    cogfunction == 3     ~ 1L,
+    TRUE                 ~ NA_integer_)) |> 
+  arrange(hhidpn, year) |>
+  group_by(hhidpn) |> 
+  # downup rather than updown: has to do with row ordering not time.
+  tidyr::fill(cog_dementia, .direction = "downup") |> 
+  mutate(state = cummax(cog_dementia)) |> 
+  ungroup()
+
+
+year_wave_map <- tibble(
+  year = seq(2000, 2022, by = 2),
+  wave = 5:16
+)
+
+cog_long <- cog_long |>
+  left_join(year_wave_map, by = join_by(year)) 
+
+# Merge cognition into HRS data and assign final stat
+# all good
+hrs_joined <- hrs_long2 |>
+  left_join(cog_long, by = c("hhidpn", "wave")) |>
+  mutate(
+    state = case_when(
+      iwstat == 5              ~ 2L,         # deceased
+      !is.na(state)            ~ state,      # dementia status
+      TRUE                     ~ NA_integer_ # NA otherwise
+    )
+  )
+
+hrs_joined %>% count(state)
+hrs_joined %>% count(iwstat)
+
+hrs_joined <- hrs_joined %>% 
+  select(-c(stroke, hibpe, diabe, hearte, 
+            cogfunction, cog_dementia, n)) |> 
+  rename(birth_date    = rabdate,
+         death_date    = raddate,
+         hypertension  = hibpe_status,
+         diabetes      = diabe_status,
+         heart_disease = hearte_status,
+         stroke        = stroke_status) |> 
+  # TR: new 25-6-2026
+  mutate(age = if_else(is.na(age), age_imputed, age)) |> 
+  select(-age_imputed) |> 
+  arrange(hhidpn, age)
+
+hrs_joined %>% 
+  select(wtcrnh) %>%
+  is.na() %>% 
+  colSums()
+
+# many weights were imputed
+hrs_joined %>% 
+  group_by(hhidpn) |> 
+  mutate(wtcrnh = if_else(wtcrnh == 0, NA, wtcrnh)) |> 
+  fill(wtcrnh, .direction = "updown") |> 
+  ungroup() %>% 
+  select(wtcrnh) %>%
+  is.na() %>% 
+  colSums()
+
+
+hrs_joined <- hrs_joined %>% 
+  group_by(hhidpn) |> 
+  mutate(wtcrnh = if_else(wtcrnh == 0, NA, wtcrnh)) |> 
+  fill(wtcrnh, .direction = "updown") |> 
+  ungroup()
+
+# nothing interesting
+hrs_msm <- hrs_joined |> 
+  # pick age range to fit to, based on plot of support.
+  # filter(between(age, 55, 97)) |> 
+  mutate(
+    int_date         = suppressWarnings(as.integer(int_date)),
+    interview_date   = as_date(int_date, origin = "1960-01-01"),
+    int_date_decimal = decimal_date(interview_date)
+  ) |> 
+  arrange(hhidpn, age) |>
+  # mutate(age_diff = age - (int_date_decimal - decimal_date(as_date(birth_date, origin = "1960-01-01")))) |> 
+#   WE DO NOT USE IT LATER?
+  mutate(
+    state_msm = state + 1  # msm expects states starting at 1
+  ) |> 
+  group_by(hhidpn) |> 
+  # supposed to remove solitary observations.
+  filter(n() > 1) |> 
+  ungroup()
+# ------------------------------------------------------------------- #
+# all filtering before here; needed to create spline 
+# basis with consistent age range in fit and in predictions.
+# spline_basis_fit is also used later for this reason
+# spline_basis_fit   <- ns(hrs_msm$age, df = 3)
+# age_splines        <- as.data.frame(spline_basis_fit)
+# names(age_splines) <- paste0("age_spline", 1:3)
+# ------------------------------------------------------------------- #
+# This second part is supposed to eliminate impossible transitions.
+
+hrs_msm <- hrs_msm |> 
+  # bind_cols(age_splines) |>
+  group_by(hhidpn) |> 
+  arrange(age) |> 
+  mutate(
+#     WHY NOT state_msm???
+    ever_dementia = cumany(state == 1),
+    state_clean   = case_when(
+      # HERE WE GO
+      state == 2                 ~ 2,  # preserve death
+      ever_dementia & state == 0 ~ 1,  # impute "recovery" as still dementia
+      TRUE ~ state
+    ),
+    died = cumany(state_clean == 2)
+  ) |>
+  filter(!(died & state_clean != 2)) |> 
+  ungroup() |> 
+  arrange(hhidpn, age) |> 
+  # treat death times as exact, but othe transition
+  # times as unknown.
+#   OK SO WE USED state_clean and here we use state_msm????
+  mutate(obstype = ifelse(state_msm == 3, 3, 1)) |>
+  # ------------------------------------------------------------------- #
+  # RT: After cleaning we have to remove a solitary obs. one more time
+  # 190 persons, they provide no information
+  group_by(hhidpn) |> 
+  # supposed to remove solitary observations.
+  filter(n() > 1) |>
+  ungroup()
+# ------------------------------------------------------------------- #
+# here we will work with NA values in the data
+nas <- hrs_msm |>
+  is.na() |>
+  colSums()
+
+# 360 misses in race are legit. 
+# 54 in education too
+# there are 25287 entries with unknown int_date
+# these cases are also missing the health covariate info
+# but they have a state info
+# What should we do with such cases?
+# TR: states were interpolated for these cases. To the extent that health covariates
+# are to be treated as "ever" or "destined to / ever" variables, then we should 
+# impute.
+# nas[nas > 0]
+# ------------------------------------------------------------------- #
+# Since all health conditions are "ever had" 
+# We can obtain extra 28181 from 99887 missing cases
+# by grouping and filling
+# Here we fill as ever had. Can be dangerous??
+hrs_msm <- hrs_msm |>
+  group_by(hhidpn) |>
+  fill(c(hypertension, diabetes, 
+         heart_disease, stroke),  .direction = "downup") |>
+  ungroup()
+# ------------------------------------------------------------------- #
+# I suggest using newly created obs_date instead of int_date
+# hrs_msm |> 
+#   filter(!is.na(int_date)) |>  
+#   is.na() |> 
+#   colSums()
+
+# create obs_date code by TR
+hrs_to_fit <- hrs_msm |>
+  # a test condition to check pandemic effect on mort trend
+  mutate(
+    birth_date         = as_date(birth_date, origin = "1960-01-01"),
+    birth_date_decimal = decimal_date(birth_date),
+    obs_date           = birth_date_decimal + age
+  ) |>
+  filter(obs_date < (as_date("2019-dec-31") |> decimal_date())) |>
+  # factor variables
+  mutate(across(c(female, race, hispanic, education,
+                  hypertension, diabetes, heart_disease,
+                  stroke, ever_dementia), ~ as.factor(.))) |> 
+  filter(wave >= 5)
+
+hrs_to_fit %>% 
+  count(round(obs_date), wave)
+
+# ------------------------------------------------------------------- #
+# Remove unneeded objects: we need to manage memory if bootstrapping! #
+# ------------------------------------------------------------------- #
+rm(list=setdiff(ls(), "hrs_to_fit"))
+gc()
+# end prep
+
+hrs_to_fit_prepped <- 
+  hrs_to_fit |>
+  mutate(period5 = case_when(between(obs_date, 2004, 2010) ~ "period 1",
+                             between(obs_date, 2010, 2015) ~ "period 2",
+                             obs_date > 2015 ~ "period 3",
+                             TRUE ~ "other"),
+         year = floor(obs_date)) |>
+  select(hhidpn, female, education,
+         pwt = wtcrnh, age, hypertension:stroke,
+         state_msm, ever_dementia, obstype, period5,
+         year) |> 
+  filter(period5 != "other",
+         year > 2003,
+#          AND AGAIN WE USE state_msm
+         state_msm != 3) |> 
+  mutate(period = as.factor(period5))
